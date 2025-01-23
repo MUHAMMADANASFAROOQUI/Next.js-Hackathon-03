ca447069beb18ba13d7c5ce51d280d35abab3b08
MUHAMMADANASFAROOQUI <164450451+MUHAMMADANASFAROOQUI@users.noreply.github.com>
2025-01-23 16:02:04 +0500

    Add files via upload

---
+++ Project/next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['cdn.sanity.io', 'localhost:3001'], // Add your external domain here
+    },
+
+  
+  };
+  
+  export default nextConfig;
+  
---
+++ Project/tailwind.config.ts
@@ -0,0 +1,75 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      cletterSpacing: {
+  			percent: '`-0.03em`',
+  			percent1: '`-0.02em`'
+  		},
+  		colors: {
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			primary1: '#3563E9',
+  			information: '#54A6FF',
+  			secondary1: '#1A202C',
+  			secondary3: '#90A3BF',
+			primary3: "#F6F7F9",
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			text1: '#90A3BF',
+  			text2: '#1A202C',
+  			text3: '#131313',
+			
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+    },
+  },
+  plugins: [],
+};
+export default config;
